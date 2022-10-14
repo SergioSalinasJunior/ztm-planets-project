c22db0601af616c4e7f4e0a0ff5667c0c562892d
Sergio Salinas Junior <104912672+SergioSalinasJunior@users.noreply.github.com>
2022-10-14 12:43:56 -0300

    Read Stream (no parsing)
    index.js update to read the kepler data as a stream.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+/*
+    importing modules needed for this file
+    -csv parse is a third-part node module
+    -fs is the file system of the node module built in packages (commonJS)
+*/
 const parse = require('csv-parse');
+const fs = require('fs');
+
+//array to hold the values read from the kepler_data
+const results = [];
+
+/*
+    Open the file as a strem (event emitter)
+    1. push data received from the kepler data to our results array
+    2. error handling for mispeling of the file path
+    3. at the end os the file print the results data and "done" message
+*/
+fs.createReadStream('kepler_data');
+    .on('data', (data) => {
+        results.push(data);
+    })
+    .on('error', (err) => {
+        console.log(err);
+    })
+    .on('end', () => {
+        console.log(results);
+        console.log('done');
+    });
